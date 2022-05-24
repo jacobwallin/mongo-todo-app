ef4fa87fcf3c1ee550eb9d65d927d9dead390092
Jacob Wallin <jacobwallin@gmail.com>
2022-05-23 21:36:22 -0500

    add timestamps

--- server/model.js
+++ server/model.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const todoSchema = new Schema({
-  ip: {
-    type: String,
-    required: true,
+const todoSchema = new Schema(
+  {
+    ip: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Todo = mongoose.model("todo", todoSchema);
 

c49de26c24f222f91e8b8d555e2a5ba67f2c165e
Jacob Wallin <jacobwallin@gmail.com>
2022-05-24 00:13:51 -0500

    whoops

--- server/database.js
+++ server/database.js
@@ -3,7 +3,5 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log("*** Error connected to database ***", err));
 
 async function main() {
-  await mongoose.connect(
-    `mongodb+srv://jacob:jwrzRvRSDnT6JoYD@cluster0.ifajw.mongodb.net/todo?retryWrites=true&w=majority`
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 }
